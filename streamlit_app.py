6b1fc14e7398fc6e0f400b22a088e35c796cd537
HalSmith181 <166065458+HalSmith181@users.noreply.github.com>
2024-07-26 18:47:08 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,13 @@ dwarf_names_df = pd.DataFrame({
 
 type = st.radio("Choose a species:" ['Elf', 'Dwarf', 'Goblin'])
 
-if type = 'Elf':
+if type == 'Elf':
     for i in range(name_qty):
         elf_random_start = elf_names_df['start'].sample(n = 1).values[0]
         elf_random_middle = elf_names_df['middle'].sample(n = 1).values[0]
         elf_random_end = elf_names_df['end'].sample(n = 1).values[0]
         st.write(f'{elf_random_start}{elf_random_middle}{elf_random_end}')
-elif type = 'Goblin':
+elif type == 'Goblin':
     for i in range(name_qty):
         gob_random_start = goblin_names_df['start'].sample(n = 1).values[0]
         gob_random_middle = goblin_names_df['middle'].sample(n = 1).values[0]
